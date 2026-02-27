e90a66f8678fe6c418730e73ca949c662e980e6a
farisantoni <farisdanish.antoni@gmail.com>
2026-02-28 07:35:28 +0800

    chore(user): add developer role

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String,
-    role: { type: String, enum: ['admin', 'agent', 'merchant', 'user'], default: 'user' },
+    role: { type: String, enum: ['developer','admin', 'agent', 'merchant', 'user'], default: 'user' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
