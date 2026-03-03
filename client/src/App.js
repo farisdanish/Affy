8ead5075d0116e6e779a569a9be259a1a696dea4
farisantoni <farisdanish.antoni@gmail.com>
2026-03-03 14:38:38 +0800

    chore(analytics): implement vercel analytics to project

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         </Router>
       </AuthProvider>
       <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   );
 };
