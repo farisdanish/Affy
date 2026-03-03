93b8380e1857f2155168534679cf24f8c30c7385
Faris Danish <56627405+farisdanish@users.noreply.github.com>
2026-03-03 09:48:43 +0800

    feat(server-config): update cors() to allow for both local and production

--- server/server.js
+++ server/server.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://affy-three.vercel.app'
+  ]
+}));
+
 app.use(express.json());
 
 // Connect to MongoDB
